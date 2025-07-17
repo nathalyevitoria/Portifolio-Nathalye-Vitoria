33ac5de99f0771a54e5ac0bb19215a6464d7e4fa
Lucas Leria <lucasleria17@gmail.com>
2025-07-17 10:08:29 -0300

    add: animações

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,27 +11,27 @@ function Contact() {
         <div className="list contact-buttons">
           <a
             href="mailto:nathalye277@gmail.com"
-            className="contact-btn text-gradient-border"
+            className="contact-btn animated-gradient-text"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="text-gradient">Email</span>
+            Email
           </a>
           <a
             href="https://api.whatsapp.com/send?phone=5511953192603&text=Ol%C3%A1!%20acessei%20seu%20site%20portfolio%20e%20gostaria%20de%20falar%20com%20voc%C3%AA!"
-            className="contact-btn text-gradient-border"
+            className="contact-btn animated-gradient-text"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="text-gradient">Telefone</span>
+            Telefone
           </a>
           <a
             href="https://www.linkedin.com/in/nathalye-vitoria-felix-17689a205/"
-            className="contact-btn text-gradient-border"
+            className="contact-btn animated-gradient-text"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="text-gradient">LinkedIn</span>
+            LinkedIn
           </a>
         </div>
       </div>
