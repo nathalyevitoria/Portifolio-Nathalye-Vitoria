267af3cfecb4f670f7cde1a7579577094997a517
Lucas Leria <lucasleria17@gmail.com>
2025-07-18 03:48:28 -0300

    add: ver certificado

--- src/pages/CertificationDetails.jsx
+++ src/pages/CertificationDetails.jsx
@@ -1,9 +1,5 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { certifications } from "../data/CertificationsData.js";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
 import React from "react";
 
 function CertificationDetails() {
@@ -30,31 +26,10 @@ function CertificationDetails() {
   return (
     <div className="certification-container">
       <div className="certification-details-modal">
-        <div className="certification-carousel-modal">
-          <Swiper
-            navigation
-            modules={[Navigation]}
-            spaceBetween={20}
-            slidesPerView={1}
-            className="media-carousel"
-          >
-            {cert.media && cert.media.map((item, index) => (
-              <SwiperSlide key={index}>
-                {item.type === "image" ? (
-                  <img
-                    src={item.src}
-                    alt={`Slide ${index}`}
-                    className="media-item"
-                  />
-                ) : (
-                  <video controls className="media-item">
-                    <source src={item.src} type="video/mp4" />
-                    Seu navegador não suporta vídeos.
-                  </video>
-                )}
-              </SwiperSlide>
-            ))}
-          </Swiper>
+        <div className="certification-image-gradient" style={{maxWidth: 600, marginBottom: 32}}>
+          <div className="certification-image-inner">
+            <img src={cert.image} alt={cert.name} />
+          </div>
         </div>
         <h2 className="certification-title-modal">{cert.name}</h2>
         <p className="certification-description-modal">{cert.description}</p>
